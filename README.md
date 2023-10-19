62655e42ca325cb6a6172b9469ee29b088856ab4
Vladyslav Kyrychuk <vkuruchuk@gmail.com>
2023-10-19 22:51:18 +0300

    Add cs fixer

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NutritionandPsychophysicalResearch
  The system of providing assistance in psychophysical condition and emotional well-being
+
+# Board
+https://trello.com/b/1r1iT73m/nutritionandpsychophysicalresearch
+
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,10 +1,11 @@
 <?php
+
 // src/Entity/User.php
 
 namespace App\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 
 /**
  * @ORM\Entity
